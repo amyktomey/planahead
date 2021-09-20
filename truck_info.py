5fc254a8310d0ac45d8d1774d84c7fc932382638
Amy Tomey <amy.tomey59@gmail.com>
2021-09-19 23:08:15 -0400

    initial commit

---
+++ truck_info.py
@@ -0,0 +1,9 @@
+#food truck locations, Monday through Friday
+location = {'Monday':'U of L',
+            'Tuedsay':'GE',
+            'Wednesday':'TPUSA',
+            'Thursday': 'Jefferson Mall',
+            'Friday':'Big 4 Bridge'}
+#weather forecast for day @zipcode
+
+#Menu pre order here (inventory assist) while loop?
