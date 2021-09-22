71594db4c67b9cea2f17d99260833f08bbd7e43c
Amy Tomey <amy.tomey59@gmail.com>
2021-09-22 16:02:20 -0400

    add JSON for truck locations, removed junk code

--- truck_info.py
+++ truck_info.py
@@ -1,10 +1,5 @@
-#food truck locations, Monday through Friday
-location = {'Monday':'U of L',
-            'Tuedsay':'GE',
-            'Wednesday':'TPUSA',
-            'Thursday': 'Jefferson Mall',
-            'Friday':'Big 4 Bridge'}
-location_zip = {40292, 40228, 40223, 40219, 40245}
+#food truck locations, Monday through Friday, closed Saturday and Sunday in JSON
+
 #get today's date...
 from datetime import datetime
 
@@ -12,21 +7,14 @@ from datetime import datetime
 now = datetime.now()
 
 # convert to string
-today = now.strftime("%A-%m-%d-%Y")
+today = now.strftime("%A %m-%d-%Y")
 day_name = now.strftime("%A")
-print('Today is ', today)
-if day_name == ('Saturday' or 'Sunday'):
-    print("I'm sorry, the food truck is closed on the weekend.")
-else:
-    day_name != ('Saturday' or 'Sunday')
-    
-    print("The food truck is at ", location.item, today)
 
-
-#food truck is closed on Saturday and Sunday
 #error if data requested on Saturday and Sunday
 
 
-#weather forecast for any calendar day by zipcode
+print('Today is', today)
+
+#weather forecast for today by zipcode
 
 #Menu pre order here (inventory assist) while loop
