61ff0afcb9f9a179ce4dfd5909fa23b6c7588b0a
Amy Tomey <amy.tomey59@gmail.com>
2021-09-21 21:18:50 -0400

    initial commit

--- truck_info.py
+++ truck_info.py
@@ -4,6 +4,12 @@ location = {'Monday':'U of L',
             'Wednesday':'TPUSA',
             'Thursday': 'Jefferson Mall',
             'Friday':'Big 4 Bridge'}
-#weather forecast for day @zipcode
+location_zip = {40292, 40228, 40223, 40219, 40245}
+#use calendar to get today's date...
+#food truck is closed on Saturday and Sunday
+#error if data requested on Saturday and Sunday
+
+
+#weather forecast for any calendar day by zipcode
 
 #Menu pre order here (inventory assist) while loop?
